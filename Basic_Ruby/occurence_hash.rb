549ed903330801096a4beea7ca700f8b6ea034ae
shiksha <shiksha@vinsol.com>
2014-06-27 20:03:34 +0530

    occurence-hash following instructions

--- Basic_Ruby/occurence_hash.rb
+++ Basic_Ruby/occurence_hash.rb
@@ -1,9 +1,9 @@
 in_string=gets
-h=Hash.new(0)
+hash=Hash.new(0)
 for i in (0 ... in_string.length)do
-h[in_string[i]]=h[in_string[i]]+1
+  hash[in_string[i]]=hash[in_string[i]]+1
 end
 puts "The Hash values stored are::"
 for i in ('a'..'z')do
-puts "The no. od occurences of #{i} is #{h[i]}"
+  puts "The no. od occurences of #{i} is #{hash[i]}"
 end
