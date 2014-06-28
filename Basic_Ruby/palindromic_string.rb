75e8d23df231f6e9945e20ecdde89672370bfde6
shiksha <shiksha@vinsol.com>
2014-06-28 18:24:25 +0530

    palindromic string using one if block

--- Basic_Ruby/palindromic_string.rb
+++ Basic_Ruby/palindromic_string.rb
@@ -1,16 +1,18 @@
-def palindrome?(string)
-  string==string.reverse
+class String 
+  def palindrome?(string)
+    string==string.reverse
+  end
 end
 
-while true do
-  input=gets
-  input=input.chomp
-  if(input=="q"||input=="Q")
-    break
-  end
-  if(palindrome?(input)) then
+input=gets
+input=input.chomp
+
+while (input!="q" and input!="Q")  do
+  if(String.new.palindrome?(input)) then
     puts "#{input} is a Palindrome"
   else
     puts "#{input} is not a Palindrome"
   end
+  input=gets
+  input=input.chomp
 end
