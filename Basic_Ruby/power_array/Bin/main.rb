5355a5e031f10755fd2c2e45cdf136dc659601c1
shiksha <shiksha@vinsol.com>
2014-06-30 15:56:08 +0530

    Power Array Exercise

---
+++ Basic_Ruby/power_array/Bin/main.rb
@@ -0,0 +1,2 @@
+require_relative "../Lib/array.rb"
+puts Array.new.power(6)
---
+++ Basic_Ruby/power_array/Lib/array.rb
@@ -0,0 +1,9 @@
+class Array
+  def power(x)
+    array=[1,2,3,4,5]
+    for i in (1...array.length) do
+      array[i]=array[i]**x
+    end
+    array
+  end
+end
