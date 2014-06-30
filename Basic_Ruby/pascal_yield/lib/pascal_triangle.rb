f90d794ea433753f0118045c74d987ba793a8fd1
shiksha <shiksha@vinsol.com>
2014-06-30 22:27:00 +0530

    Pascal's triangle

---
+++ Basic_Ruby/pascal_yield/lib/pascal_triangle.rb
@@ -0,0 +1,15 @@
+class Pascal_Triangle
+	def pascal(size)
+		arr = [1]
+		for i in (0..size)
+			yield arr
+			arr_new=Array.new(i+2)
+			arr_new[0]=1
+			for j in(1..i)
+				arr_new[j] = arr[j-1] + arr[j]
+			end
+			arr_new[i+1] = 1
+			arr = arr_new
+		end
+	end
+end
