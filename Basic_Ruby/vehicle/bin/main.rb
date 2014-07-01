1255efb3505d0af3f4b9f25e9e21db2681aec8fa
shiksha <shiksha@vinsol.com>
2014-07-01 22:36:18 +0530

    Use to_S and attr_accessor

--- Basic_Ruby/vehicle/bin/main.rb
+++ Basic_Ruby/vehicle/bin/main.rb
@@ -1,7 +1,6 @@
-require_relative '../lib/vehicle.rb'
 require_relative '../lib/bike.rb'
 
-bike_obj = Bike.new("Honda", 100000, "M. k. Tiwari")
-puts "The details of bike are : Bike-name=#{bike_obj.name} Price=#{bike_obj.price} Dealer=#{bike_obj.dealer}"
-bike_obj.price = 60000
-puts "The details of bike now are : Bike-name=#{bike_obj.name} Price=#{bike_obj.price} Dealer=#{bike_obj.dealer}"
+bike_object = Bike.new("Honda", 100000, "M. k. Tiwari")
+puts bike_object
+bike_object.price = 60000
+puts bike_object
