2f90ceba842e8fe73e3290ec7c5e039387ca3bb6
shiksha <shiksha@vinsol.com>
2014-07-01 18:36:29 +0530

    Add space around operator

--- Basic_Ruby/palindromic_string/bin/main.rb
+++ Basic_Ruby/palindromic_string/bin/main.rb
@@ -1,8 +1,8 @@
 require_relative "../lib/string.rb"
 
 input = gets.chomp
-while (/[q]/i.match(input) == nil)  do
-  if(input.palindrome?) then
+while !(input =~ /^q$/i)  do
+  if input.palindrome? then
     puts "#{input} is a Palindrome"
   else
     puts "#{input} is not a Palindrome"
--- Basic_Ruby/palindromic_string/lib/string.rb
+++ Basic_Ruby/palindromic_string/lib/string.rb
@@ -1,5 +1,5 @@
 class String 
   def palindrome?
-    reverse==self
+    reverse == self
   end
 end
