c540323e07d20f477e812acb9a2682fd34e086a5
shiksha <shiksha@vinsol.com>
2014-07-01 22:45:19 +0530

    use to_s and attr_accessor

--- Basic_Ruby/vehicle/lib/vehicle.rb
+++ Basic_Ruby/vehicle/lib/vehicle.rb
@@ -1,11 +1,15 @@
 class Vehicle
+
   attr_reader :name
   attr_accessor :price
+
   def initialize(name, price)
     @name = name
     @price = price
   end
+
   def to_s
-    "\tName : #{name}\n \tPrice : #{price}\n"
+    " \tName : #{name}\n \tPrice : #{price}\n "
   end
+
 end
