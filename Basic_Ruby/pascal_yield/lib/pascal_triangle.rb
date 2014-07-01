7c73a70825074e2626c4444d64b8f013bfaecfe8
shiksha <shiksha@vinsol.com>
2014-07-01 18:58:37 +0530

    Add spaces around brackets

--- Basic_Ruby/pascal_yield/lib/pascal_triangle.rb
+++ Basic_Ruby/pascal_yield/lib/pascal_triangle.rb
@@ -3,11 +3,13 @@ class PascalTriangle
     arr = [1]
     for i in (0..size)
       yield arr
-      arr_new=Array.new(i + 2)
-      arr_new[0]=1
-      for j in(1..i)
+      arr_new = Array.new(i + 2)
+      arr_new[0] = 1 
+
+      for j in (1..i)
         arr_new[j] = arr[j - 1] + arr[j]
       end
+      
       arr_new[i + 1] = 1
       arr = arr_new
     end
