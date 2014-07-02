372b4906411322acc052d7e33b0a423dc0e90750
shiksha <shiksha@vinsol.com>
2014-07-02 12:49:36 +0530

    Fix indentation issues

--- Basic_Ruby/hash_inject/lib/array.rb
+++ Basic_Ruby/hash_inject/lib/array.rb
@@ -1,20 +1,20 @@
 class Array
 
-	def group_by_size
-    size_array_pair = Hash.new { |hash, key| hash[key] = [] }
+  def group_by_length
+    temporary_hash = Hash.new { |hash, key| hash[key] = [] }
     each do |val|
-      size_array_pair[val.to_s.length] << val
+      temporary_hash[val.to_s.length] << val
     end
-    size_array_pair
+    temporary_hash
   end
 
   def odd_even_segregation
 
-		group_by_size.inject(Hash.new { |hash, key| hash[key] = [] } ) do |memo, (key, value)|
-		  key.odd? ? memo["odd"] << value : memo["even"] << value
-			memo
-		end
+    group_by_length.inject(Hash.new { |hash, key| hash[key] = [] } ) do |memo, (key, value)|
+      key.odd? ? memo["odd"] << value : memo["even"] << value
+      memo
+    end
 
-	end
+  end
 
 end
