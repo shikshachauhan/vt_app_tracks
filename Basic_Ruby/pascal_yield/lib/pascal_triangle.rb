b80440e9fc54b4188d8c2929e81fa0fe0199308e
shiksha <shiksha@vinsol.com>
2014-07-04 13:09:46 +0530

    Remove for lops and use times and upto

--- Basic_Ruby/pascal_yield/lib/pascal_triangle.rb
+++ Basic_Ruby/pascal_yield/lib/pascal_triangle.rb
@@ -1,17 +1,19 @@
 class PascalTriangle
   def pascal(size)
     arr = [1]
-    for i in (0..size)
+
+    (size+1).times do |i|
       yield arr
       arr_new = Array.new(i + 2)
       arr_new[0] = 1 
 
-      for j in (1..i)
+      1.upto(i) do |j|
         arr_new[j] = arr[j - 1] + arr[j]
       end
-      
+
       arr_new[i + 1] = 1
       arr = arr_new
     end
+
   end
 end
