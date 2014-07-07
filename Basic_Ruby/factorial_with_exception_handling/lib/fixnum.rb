f25e13a5d3127d714620d5e728f687bda9495f19
shiksha <shiksha@vinsol.com>
2014-07-07 11:55:27 +0530

    Handle error using custom exception and use each

--- Basic_Ruby/factorial_with_exception_handling/lib/fixnum.rb
+++ Basic_Ruby/factorial_with_exception_handling/lib/fixnum.rb
@@ -1,11 +1,14 @@
+require_relative 'negative_factorial_exception.rb'
+
 class Fixnum
   def find_factorial
     return 1 if self == 0
     begin
-      raise '-ve integer error' if self < 0
-      factorial = (1..self).inject(:*)
-
-      rescue RuntimeError
+      raise NegativeFactorialException.new if(self < 0)
+      factorial = 1
+      (1..self).each { |var| factorial *= var }
+      factorial
+      rescue NegativeFactorialException
       puts "The program can't find factorials for negative Numbers"
     end
   end
---
+++ Basic_Ruby/factorial_with_exception_handling/lib/negative_factorial_exception.rb
@@ -0,0 +1,2 @@
+class NegativeFactorialException < RuntimeError
+end
