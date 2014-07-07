adcd122888e950e7af2a5e35c5f871fb20f3fd15
shiksha <shiksha@vinsol.com>
2014-07-07 12:48:28 +0530

    Change method names, pass arguments as hash

--- Basic_Ruby/interest_difference/bin/main.rb
+++ Basic_Ruby/interest_difference/bin/main.rb
@@ -1,8 +1,9 @@
 require_relative '../lib/interest.rb'
 
-principal = gets.chomp.to_i
-time = gets.chomp.to_i
+interest_details = Hash.new
+interest_details['principal'] = gets.chomp.to_i
+interest_details['time'] = gets.chomp.to_i
 
-interest = Interest.new { [principal, time] }
+interest = Interest.new { interest_details }
 
 puts interest.simple_compound_difference
--- Basic_Ruby/interest_difference/lib/interest.rb
+++ Basic_Ruby/interest_difference/lib/interest.rb
@@ -1,24 +1,24 @@
 class Interest
 
   RATE = 0.1
-  attr_accessor :principal, :time
+  attr_reader :principal, :time
 
   def initialize
     instance_variables = yield
-    @principal = instance_variables[0]
-    @time = instance_variables[1]
+    @principal = instance_variables['principal']
+    @time = instance_variables['time']
   end
 
-  def simple_amount
+  def amount_simply
     principal + principal * RATE * time
   end
 
-  def compound_amount
-    principal * ((1 + RATE) ** time) 
+  def amount_compoundedly
+    principal * ((1 + RATE) ** time)
   end
 
   def simple_compound_difference
-    compound_amount - simple_amount
+    amount_compoundedly - amount_simply
   end
 
 end
