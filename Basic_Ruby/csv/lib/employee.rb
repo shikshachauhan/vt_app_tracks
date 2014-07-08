7f564833cf103151ec5b9fd27a03e709c74697d7
shiksha <shiksha@vinsol.com>
2014-07-08 16:09:04 +0530

    Make pluralize class and initialize employee

--- Basic_Ruby/csv/lib/employee.rb
+++ Basic_Ruby/csv/lib/employee.rb
@@ -1,6 +1,9 @@
 class Employee
   attr_accessor :name, :emp_id, :designation
+  def initialize(name, emp_id, designation)
+    @name, @emp_id, @designation = name, emp_id, designation
+  end
   def to_s
-    "#{name}(EmpId:#{emp_id})"
+    "#{ name }(EmpId:#{ emp_id })"
   end
 end
--- Basic_Ruby/csv/lib/format_converter.rb
+++ Basic_Ruby/csv/lib/format_converter.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require_relative 'employee.rb'
+require_relative 'pluralize.rb'
 
 class FormatConverter
 
@@ -7,10 +8,7 @@ class FormatConverter
   def store_details(input_file)
     @employee_datails = Hash.new { |hash, key| hash[key] = Array.new }
     CSV.foreach(File.join(BASE_PATH, input_file), { headers: true } ) do |row|
-      employee = Employee.new
-      employee.name = row[0]
-      employee.emp_id = row[1].strip
-      employee.designation = row[2].strip
+      employee = Employee.new(row[0], row[1].strip, row[2].strip)
       @employee_datails[employee.designation.downcase.to_sym].push(employee)
     end
   end
@@ -19,8 +17,7 @@ class FormatConverter
     File.open(File.join(BASE_PATH, output_file), 'w') do |file|
       @employee_datails.each do |key, array|
         file.puts
-        file.print "#{ key.capitalize }"
-        (array.length > 1) ? file.puts('s') : (file.puts)
+        file.puts Pluralize.new.pluralize(key.capitalize.to_s, array.length)
         file.puts array
       end
     end
