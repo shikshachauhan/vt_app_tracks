c117fa22dc70ab3b02d767783deed5221e4e2afc
shiksha <shiksha@vinsol.com>
2014-07-18 17:50:03 +0530

    check box exercise

---
+++ java_script/check_all_or_none/js/check_box.js
@@ -0,0 +1,16 @@
+function Checkbox() {
+}
+Checkbox.prototype.mark = function(value) {
+  var checkBox = document.getElementsByClassName("checkbox");
+  for(var i = 0; i < checkBox.length; i++) {
+    checkBox[i].checked = value;
+  }
+}
+window.onload = function() {
+  document.getElementById("check").onclick = function() {
+    Checkbox.prototype.mark(true);
+  }
+  document.getElementById("uncheck").onclick = function() {
+    Checkbox.prototype.mark(false);
+  }
+}
