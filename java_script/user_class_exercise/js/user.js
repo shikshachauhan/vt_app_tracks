ca8d4eb19d30d635a2773a4eb9d8ae99b0a97437
shiksha <shiksha@vinsol.com>
2014-07-18 14:00:54 +0530

    validate age and enhance modularity

--- java_script/user_class_exercise/js/user.js
+++ java_script/user_class_exercise/js/user.js
@@ -1,11 +1,34 @@
 function User(name, age) {
   this.name = name;
-  this.age = age;
+  if(this.isValidAge(age)) {
+    this.age = age;
+  }
+}
+User.prototype.agePattern = /^(([1-9]\d?)|(1[0-1]\d))$/;
+User.prototype.isValidAge = function(age) {
+  return this.agePattern.test(age);
+}
+User.prototype.comparisonOutput = function(name, cmpString) {
+  return this.name + cmpString + name;
 }
 User.prototype.compare = function(user) {
-  return (this.age > user.age) ? this.name + 'is older than' + user.name :
-  user.name + ' is older than ' + this.name;
+  if(this.age && user.age) {
+    if(this.age > user.age) {
+      return this.comparisonOutput(user.name, ' is older than ');
+    }
+    else if(this.age < user.age) {
+      return this.comparisonOutput(user.name, ' is younger than ');
+    }
+    else {
+      return this.comparisonOutput(user.name, ' has same age as ');
+    }
+  }
+  else{
+    return 'Either user has an infeasible age';
+  }
 }
-var user1 = new User('Shiksha', 12);
-var user2 = new User('Sakshi', 22);
-document.getElementById('user_compare').innerHTML = user1.compare(user2);
+window.onload = function() {
+  var user1 = new User('Shiksha', 12);
+  var user2 = new User('Sakshi', 22);
+  document.getElementById('user_compare').innerHTML = user1.compare(user2);
+}
