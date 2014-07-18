4de5feff0e895cf7cc38153c6bc304b4467493c6
shiksha <shiksha@vinsol.com>
2014-07-18 18:29:59 +0530

    change double quotes to singles

--- java_script/check_all_or_none/js/check_box.js
+++ java_script/check_all_or_none/js/check_box.js
@@ -1,16 +1,16 @@
 function Checkbox() {
 }
 Checkbox.prototype.mark = function(value) {
-  var checkBox = document.getElementsByClassName("checkbox");
+  var checkBox = document.getElementsByClassName('checkbox');
   for(var i = 0; i < checkBox.length; i++) {
     checkBox[i].checked = value;
   }
 }
 window.onload = function() {
-  document.getElementById("check").onclick = function() {
+  document.getElementById('check').onclick = function() {
     Checkbox.prototype.mark(true);
   }
-  document.getElementById("uncheck").onclick = function() {
+  document.getElementById('uncheck').onclick = function() {
     Checkbox.prototype.mark(false);
   }
 }
