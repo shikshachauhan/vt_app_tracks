365f4c8301cfd56f3a75c291ad5778329f32cd31
shiksha <shiksha@vinsol.com>
2014-07-20 13:17:25 +0530

    change buttons to links and move checkbox prototype to a variable

--- java_script/check_all_or_none/js/check_box.js
+++ java_script/check_all_or_none/js/check_box.js
@@ -1,17 +1,18 @@
 function Checkbox() {
 }
-Checkbox.prototype.mark = function(value) {
+var checkBoxPrototype = Checkbox.prototype
+checkBoxPrototype.mark = function(value) {
   var checkBox = document.getElementsByName('color');
   for(var i = 0; i < checkBox.length; i++) {
     checkBox[i].checked = value;
   }
 }
-Checkbox.prototype.addEventHandler = function() {
+checkBoxPrototype.addEventHandler = function() {
   document.getElementById('check').onclick = function() {
-    Checkbox.prototype.mark(true);
+    checkBoxPrototype.mark(true);
   }
   document.getElementById('uncheck').onclick = function() {
-    Checkbox.prototype.mark(false);
+    checkBoxPrototype.mark(false);
   }
 }
-window.onload = Checkbox.prototype.addEventHandler;
+window.onload = checkBoxPrototype.addEventHandler;
