9f84b5792c3b28fdc90f25d5a10d91684388d9f2
shiksha <shiksha@vinsol.com>
2014-07-23 15:08:24 +0530

    alert input name

---
+++ java_script/alert_name_input/js/alert_name_input.js
@@ -0,0 +1,29 @@
+function Name(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.nameString = 'Hello ' + this.firstName + ' ' + this.lastName ;
+}
+Name.prototype.writeName = function() {
+  document.body.innerHTML = this.nameString;
+}
+Name.prototype.alertName = function() {
+  alert(this.nameString);
+}
+Name.prototype.valid = function(firstName, lastName) {
+  return firstName && lastName && firstName.trim() && lastName.trim();
+}
+Name.prototype.notifyName = function() {
+  if(this.valid(this.firstName, this.lastName)) {
+    this.alertName();
+    this.writeName();
+  }
+  else {
+    alert('Invalid name');
+  }
+}
+window.onload = function() {
+  var firstName = prompt("Please enter first your name");
+  var lastName = prompt("Please enter last your name");
+  user = new Name(firstName, lastName);
+  user.notifyName();
+}
