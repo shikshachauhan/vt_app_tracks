ed8cbd7d2c3129e643003c511da52b190a076480
shiksha <shiksha@vinsol.com>
2014-07-25 13:00:37 +0530

    change variable names and enable multiple movement

--- java_script/move_countries/js/move_countries.js
+++ java_script/move_countries/js/move_countries.js
@@ -1,26 +1,28 @@
-function SelectCountries(list, choosenList, add, remove) {
-  this.list = list;
-  this.choosenList = choosenList;
+function MoveItems(list1, list2, add, remove) {
+  this.list1 = list1;
+  this.list2 = list2;
   this.add = add;
   this.remove = remove;
 }
-SelectCountries.prototype.move = function(list1, list2) {
-  list2.appendChild(list1.options[list1.selectedIndex]);
+MoveItems.prototype.move = function(list1, list2) {
+  while(list1.selectedIndex != -1) {
+    list2.appendChild(list1.options[list1.selectedIndex]);
+  }
 }
-SelectCountries.prototype.bindEvents = function() {
+MoveItems.prototype.bindEvents = function() {
   var countries = this;
   this.add.addEventListener('click', function() {
-    countries.move(countries.list, countries.choosenList);
+    countries.move(countries.list1, countries.list2);
   });
   this.remove.addEventListener('click', function() {
-    countries.move(countries.choosenList, countries.list);
+    countries.move(countries.list2, countries.list1);
   });
 }
 window.onload = function() {
-  var list = document.getElementById('list');
-  var choosenList = document.getElementById('choosen_list');
-  var add = document.getElementById('add');
-  var remove = document.getElementById('remove');
-  countries = new SelectCountries(list, choosenList, add, remove);
+  var list1 = document.getElementById('list1'),
+      list2 = document.getElementById('list2'),
+      add = document.getElementById('add'),
+      remove = document.getElementById('remove'),
+      countries = new MoveItems(list1, list2, add, remove);
   countries.bindEvents();
 }
