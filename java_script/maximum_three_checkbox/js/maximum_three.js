8bb1d628ce84a77d20a8cb462a21c83f1fdfecd6
shiksha <shiksha@vinsol.com>
2014-07-26 11:36:19 +0530

    change names, use array to store checkeda and improve conditions

--- java_script/maximum_three_checkbox/js/maximum_three.js
+++ java_script/maximum_three_checkbox/js/maximum_three.js
@@ -1,59 +1,62 @@
-var Checkbox = {
-  checkBox : document.getElementsByName('day'),
-  none : document.getElementById('none'),
-  counter : 0,
-  getCheckedDays : function() {
-    var checkedDays = []
-    for(var i = 0; i < Checkbox.checkBox.length; i++) {
-      if(Checkbox.checkBox[i].checked) {
-        checkedDays.push(Checkbox.checkBox[i].value);
-      }
-    }
-    return checkedDays;
-  },
-  alertMessage : function(checkedDays) {
-    alert('Only 3 days can be selected.\nYou have already selected '
-        + checkedDays[0] + ', ' + checkedDays[1] + ' and ' + checkedDays[2]);
-  },
-
-  //checks if more than three checkboxes are marked and takes required actions
-
-  check_greater_than_three : function() {
-    if(this.checked) {
-      Checkbox.counter++;
-      if(Checkbox.counter == 4) {
-        this.checked = false;
-        Checkbox.counter--;
-        var checkedDays = Checkbox.getCheckedDays();
-        Checkbox.alertMessage(checkedDays);
-      }
-    }
-    else {
-      Checkbox.counter--;
-    }
-  },
-  uncheckAll : function() {
-    for(var i = 0; i < Checkbox.checkBox.length; i++) {
-      Checkbox.checkBox[i].checked = false;
-    }
-    Checkbox.counter = 0;
-  },
-  uncheckNone : function() {
-    if(Checkbox.counter == 1) {
-      Checkbox.none.checked = false;
-    }
-  },
-  addEventHandler : function() {
-    for(var i = 0; i < Checkbox.checkBox.length; i++) {
-      Checkbox.checkBox[i].addEventListener
-          ('change', Checkbox.check_greater_than_three, false);
-      Checkbox.checkBox[i].addEventListener
-          ('change', Checkbox.uncheckNone, false);
-    }
-    Checkbox.none.onchange = Checkbox.uncheckAll;
+function CheckboxGroup(checkBoxes, none) {
+  this.checkBoxes = checkBoxes ;
+  this.none = none ;
+  this.checkedBoxes = [];
+}
+CheckboxGroup.prototype.alertMessage = function() {
+  alert('Only 3 days can be selected.\nYou have already selected '
+    + this.checkedBoxes[0].value + ', ' + this.checkedBoxes[1].value +
+    ' and ' + this.checkedBoxes[2].value);
+}
+CheckboxGroup.prototype.addChecked = function(event) {
+  if(event.target.checked) {
+    this.none.checked = false;
+    this.checkedBoxes.push(event.target);
   }
-};
+}
+CheckboxGroup.prototype.checkOverflow = function(event) {
+  if(this.checkedBoxes.length == 4) {
+    event.target.checked = false;
+    this.alertMessage();
+  }
+}
+CheckboxGroup.prototype.removeChecked = function(event) {
+  if(!event.target.checked) {
+    var index = this.checkedBoxes.indexOf(event.target);
+    this.checkedBoxes.splice(index, 1);
+  }
+}
+CheckboxGroup.prototype.uncheckAll = function() {
+  for(var i in this.checkedBoxes) {
+    this.checkedBoxes[i].checked = false;
+  }
+  this.checkedBoxes = [];
+}
+CheckboxGroup.prototype.bindEvents = function() {
+  var lenght = this.checkBoxes.length,
+      checkboxGroup = this;
+  for(var i = 0; i < lenght; i++) {
+    this.checkBoxes[i].addEventListener('change', function(event) {
+      checkboxGroup.addChecked(event);
+    });
+    this.checkBoxes[i].addEventListener('change', function(event) {
+      checkboxGroup.checkOverflow(event);
+    });
+    this.checkBoxes[i].addEventListener('change', function(event) {
+      checkboxGroup.removeChecked(event);
+    });
+
+  }
+  this.none.addEventListener('change', function() {
+    checkboxGroup.uncheckAll();
+  });
+}
+CheckboxGroup.prototype.init = function() {
+  this.none.checked = true;
+}
 window.onload = function() {
-  Checkbox.addEventHandler();
-  Checkbox.none.checked = true;
+  var days = document.getElementsByName('day'),
+      weekDays = new CheckboxGroup(days, none);
+  weekDays.init();
+  weekDays.bindEvents();
 }
