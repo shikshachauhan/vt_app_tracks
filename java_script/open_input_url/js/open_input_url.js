539374fc0192a2c944249995075bf608ad6c3f2f
shiksha <shiksha@vinsol.com>
2014-07-26 14:30:59 +0530

    location hash and query search added to url pattern

--- java_script/open_input_url/js/open_input_url.js
+++ java_script/open_input_url/js/open_input_url.js
@@ -1,21 +1,25 @@
 function Window(url) {
   this.url = url;
 }
-Window.prototype.urlPattern =
-/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)\/?$/i ;
-Window.prototype.isValid = function(url) {
-  return Boolean(url && this.urlPattern.test(url.trim()));
+Window.prototype.urlPattern = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)\/?(\?([\w\d_]+=[\w\d_\+]+)(&[\w\d_]+=[\w\d_\+]+)*)?(#[\w\d_]+)?$/i ;
+
+Window.prototype.isValid = function() {
+  return Boolean(this.url && this.urlPattern.test(this.url.trim()));
 }
-Window.prototype.tryOpen = function(url) {
-  if(this.isValid(url)) {
-    open(url, '_blank', 'width=400px, height=450px,' +
+
+Window.prototype.tryOpen = function() {
+  if(this.isValid()) {
+    open(this.url, '_blank', 'width=400px, height=450px,' +
       'menubar=no, scrollbars=no, status=no, title=no');
   } else {
     alert('input a valid URL');
   }
 }
+
 window.onload = function() {
   var url = prompt('input required URL');
-  var windowObject = new Window(url);
-  windowObject.tryOpen(url);
+  if(url) {
+    var windowObject = new Window(url);
+    windowObject.tryOpen();
+  }
 }
