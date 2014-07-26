6bb9b6cc6f9a8e3c78231d67c31c361cd54bb745
shiksha <shiksha@vinsol.com>
2014-07-26 12:26:05 +0530

    Remove patterns

--- java_script/registration_form/js/registration_form.js
+++ java_script/registration_form/js/registration_form.js
@@ -6,10 +6,6 @@ function RegistrationForm(form, confirm, aboutMe, textFields, email, homepage) {
   this.email = email;
   this.homepage = homepage;
 };
-RegistrationForm.prototype.emailPattern =
-/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i ;
-RegistrationForm.prototype.urlPattern =
-/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/i ;
 RegistrationForm.prototype.checkEmpty = function(event) {
   event.preventDefault();
   for(var i in this.textFields) {
