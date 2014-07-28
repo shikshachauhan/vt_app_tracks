efbb17cc0a971da3acbdb8ad2ddb31615fdc021f
shiksha <shiksha@vinsol.com>
2014-07-28 11:27:42 +0530

    pass form elements as JSON object and change class name

--- java_script/move_countries/js/move_countries.js
+++ java_script/move_countries/js/move_countries.js
@@ -1,28 +1,30 @@
-function MoveItems(list1, list2, add, remove) {
-  this.list1 = list1;
-  this.list2 = list2;
-  this.add = add;
-  this.remove = remove;
+function Items(elements) {
+  this.list1 = elements.firstList;
+  this.list2 = elements.secondList;
+  this.add = elements.addButton;
+  this.remove = elements.removeButton;
 }
-MoveItems.prototype.move = function(list1, list2) {
+Items.prototype.move = function(list1, list2) {
   while(list1.selectedIndex != -1) {
     list2.appendChild(list1.options[list1.selectedIndex]);
   }
 }
-MoveItems.prototype.bindEvents = function() {
-  var countries = this;
-  this.add.addEventListener('click', function() {
-    countries.move(countries.list1, countries.list2);
+Items.prototype.bindEvents = function() {
+  var _this = this;
+  _this.add.addEventListener('click', function() {
+    _this.move(_this.list1, _this.list2);
   });
-  this.remove.addEventListener('click', function() {
-    countries.move(countries.list2, countries.list1);
+  _this.remove.addEventListener('click', function() {
+    _this.move(_this.list2, _this.list1);
   });
 }
 window.onload = function() {
-  var list1 = document.getElementById('list1'),
-      list2 = document.getElementById('list2'),
-      add = document.getElementById('add'),
-      remove = document.getElementById('remove'),
-      countries = new MoveItems(list1, list2, add, remove);
+  var formElements = {
+    firstList: list1,
+    secondList: list2,
+    addButton: add,
+    removeButton: remove
+  };
+  var countries = new Items(formElements);
   countries.bindEvents();
 }
