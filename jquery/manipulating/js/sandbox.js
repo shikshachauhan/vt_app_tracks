f9c6bbf37a91976d9d07d8e1b5c2c68cd2f21401
shiksha <shiksha@vinsol.com>
2014-07-31 16:11:21 +0530

    better use append, prepend and add class to div

--- jquery/manipulating/js/sandbox.js
+++ jquery/manipulating/js/sandbox.js
@@ -1,9 +1,13 @@
 $(function() {
 
   //Add five new list items to the end of the unordered list #myList.
-  for(var i = 0; i < 5; i++) {
-    $('#myList').append('<li>New List Item ' + (i + 1) + '</li>')
-  }
+    $('#myList').append(function() {
+      var temp = '';
+      for(var i = 1; i < 6; i++) {
+        temp += '<li>New List Item ' + i + ' </li>' ;
+      }
+      return temp;
+    });
 
   //Remove the odd list items
   $('#myList li:odd').remove();
@@ -11,16 +15,19 @@ $(function() {
   //Add another h2 and another paragraph to the last div.module
   $('div.module')
     .last()
-      .prepend('<p>Now you can choose the day of the week.</p>')
-      .prepend('<h2>Here is something sepecial for you</h2>');
+      .prepend('<h2>Here is something sepecial for you</h2>',
+        '<p>Now you can choose the day of the week.</p>');
 
   // Add another option to the select element; give the option the value "Wednesday"
   $('<option>', {
     html: 'Wednesday',
     value: 'Wednesday'
-  }).appendTo('[name="day"]');
+  }).appendTo('select[name="day"]');
 
   //Add a new div.module to the page after the last one;
   //put a copy of one of the existing images inside of it.
-  $('div.module').last().after($('<div>', { html: $("img").eq(0).clone() }));
+  $('div.module').last().after($('<div>', { 
+    html: $("img").eq(0).clone(),
+    class: 'module'
+  }));
 });
