9eb7bb83f2555a404e1dbc6d3c17a716ad3baf15
shiksha <shiksha@vinsol.com>
2014-07-31 18:57:19 +0530

    Refine modularity

--- jquery/create_input_hint/js/inputHint.js
+++ jquery/create_input_hint/js/inputHint.js
@@ -1,48 +1,46 @@
-function InputHint(searchBox, label, hintClass) {
-  this.searchBox = searchBox;
-  this.label = label;
-  this.hintClass = hintClass;
+function InputHint(elements) {
+  this.searchBox = elements.searchBox;
+  this.label = elements.label;
 }
 InputHint.prototype.setHint = function() {
-  this.searchBox.val(this.label.text());
+  this.searchBox.val(this.label.text())
+    .addClass('hint');
 }
-InputHint.prototype.addHintClass = function() {
-  this.searchBox.addClass(this.hintClass);
-}
-InputHint.prototype.removeLabel = function() {
-  this.label.remove();
-};
 InputHint.prototype.removeHint = function() {
-  var _this = this;
-  this.searchBox.on('focus', function() {
-    $(_this.searchBox)
+  if(this.searchBox.val() == this.label.text()) {
+    this.searchBox
       .val('')
-      .removeClass(_this.hintClass);
-  })
+      .removeClass('hint');
+  }
 }
-InputHint.prototype.restoreHint = function() {
-  var _this = this;
-  this.searchBox.on('blur', function() {
-    _this.setHint();
-    _this.addHintClass();
-  })
-}
-$(function() {
-  var inputHint = new InputHint($('input[name="q"]'), $('label[for="q"]'), 'hint');
-
+InputHint.prototype.manageInputHint = function() {
   //Set the value of the search input to the text of the label element
-  inputHint.setHint();
-
   //Add a class of "hint" to the search input
-  inputHint.addHintClass();
+  var _this = this;
+  this.setHint();
 
   //Remove the label element
-  inputHint.removeLabel()
+  this.label.remove();
 
   //Bind a focus event to the search input that removes the hint text and the "hint" class
-  inputHint.removeHint();
+  this.searchBox.on('focus', function() {
+    _this.removeHint();
+  });
 
   //Bind a blur event to the search input that restores the hint text and "hint" class
   //if no search text was entered
-  inputHint.restoreHint();
+  this.searchBox.on('blur', function() {
+    if(!_this.searchBox.val().trim()) {
+      _this.setHint();
+    }
+  });
+}
+$(function() {
+  var hintElements = {
+    searchBox: $('input[name="q"]'),
+    label: $('label[for="q"]')
+  }
+  var inputHint = new InputHint(hintElements);
+
+  inputHint.manageInputHint();
 });
