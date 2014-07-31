350bcf13a03eb605d2c62735dbc3d3a73837a283
shiksha <shiksha@vinsol.com>
2014-07-31 13:46:33 +0530

    use onject orientation

--- jquery/create_input_hint/js/inputHint.js
+++ jquery/create_input_hint/js/inputHint.js
@@ -1,30 +1,48 @@
+function InputHint(searchBox, label, hintClass) {
+  this.searchBox = searchBox;
+  this.label = label;
+  this.hintClass = hintClass;
+}
+InputHint.prototype.setHint = function() {
+  this.searchBox.val(this.label.text());
+}
+InputHint.prototype.addHintClass = function() {
+  this.searchBox.addClass(this.hintClass);
+}
+InputHint.prototype.removeLabel = function() {
+  this.label.remove();
+};
+InputHint.prototype.removeHint = function() {
+  var _this = this;
+  this.searchBox.on('focus', function() {
+    $(_this.searchBox)
+      .val('')
+      .removeClass(_this.hintClass);
+  })
+}
+InputHint.prototype.restoreHint = function() {
+  var _this = this;
+  this.searchBox.on('blur', function() {
+    _this.setHint();
+    _this.addHintClass();
+  })
+}
 $(function() {
+  var inputHint = new InputHint($('input[name="q"]'), $('label[for="q"]'), 'hint');
 
   //Set the value of the search input to the text of the label element
-  var labelText = $('[for="q"]').text();
-  $('[name="q"]')
-    .val(labelText);
+  inputHint.setHint();
 
   //Add a class of "hint" to the search input
-  $('[name="q"]')
-    .addClass('hint');
+  inputHint.addHintClass();
 
   //Remove the label element
-  $('[for="q"]')
-    .remove();
+  inputHint.removeLabel()
 
   //Bind a focus event to the search input that removes the hint text and the "hint" class
-  $('[name="q"]').on('focus', function() {
-    $(this)
-      .val('')
-      .removeClass('hint');
-  })
+  inputHint.removeHint();
 
   //Bind a blur event to the search input that restores the hint text and "hint" class
   //if no search text was entered
-  $('[name="q"]').on('blur', function() {
-    $(this)
-      .val(labelText)
-      .addClass('hint');
-  })
+  inputHint.restoreHint();
 });
