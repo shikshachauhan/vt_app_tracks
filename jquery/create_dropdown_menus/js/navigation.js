221361eb8db417c9f8fa329b6a474c524c6034a5
shiksha <shiksha@vinsol.com>
2014-08-01 17:27:02 +0530

    display dropdown menus on hover in mainlist

---
+++ jquery/create_dropdown_menus/js/navigation.js
@@ -0,0 +1,31 @@
+/*Hovering over an item in the main menu should 
+*show that item's submenu items, if any.
+*Exiting an item should hide any submenu items.*/
+
+function DropdownMenu(elements) {
+  this.mainList = elements.mainList;
+}
+
+DropdownMenu.prototype.showSubMenu = function() {
+  $(this)
+    .find('ul')
+      .css('display', 'block');
+}
+
+DropdownMenu.prototype.hideSubMenu = function() {
+  $(this)
+    .find('ul')
+      .css('display', 'none');
+}
+
+DropdownMenu.prototype.init = function() {
+  this.mainList.hover(this.showSubMenu, this.hideSubMenu);
+}
+
+$(function() {
+  var menuElements = {
+    mainList: $('#nav li')
+  };
+  dropdownMenu = new DropdownMenu(menuElements);
+  dropdownMenu.init();
+});
