acf1c61eeca74b497f265aa87cb433f8852cbb76
shiksha <shiksha@vinsol.com>
2014-08-01 10:44:05 +0530

    change method name, bind events in a separate method

--- jquery/create_input_hint/js/inputHint.js
+++ jquery/create_input_hint/js/inputHint.js
@@ -1,39 +1,46 @@
 function InputHint(elements) {
   this.searchBox = elements.searchBox;
   this.label = elements.label;
+  this.hintText = elements.label.text();
 }
+
 InputHint.prototype.setHint = function() {
-  this.searchBox.val(this.label.text())
+  this.searchBox.val(this.hintText)
     .addClass('hint');
 }
+
 InputHint.prototype.removeHint = function() {
-  if(this.searchBox.val() == this.label.text()) {
+  if(this.searchBox.val() == this.hintText) {
     this.searchBox
       .val('')
       .removeClass('hint');
   }
 }
-InputHint.prototype.manageInputHint = function() {
+
+InputHint.prototype.bindEvents = function() {
+  //Bind a focus event to the search input that removes the hint text and the "hint" class
+
+  //Bind a blur event to the search input that restores the hint text and "hint" class
+  //if no search text was entered
+  var _this = this;
+  this.searchBox.on('focus', function() {
+    _this.removeHint();
+  }).on('blur', function() {
+    if(!_this.searchBox.val().trim()) {
+      _this.setHint();
+    }
+  });
+}
+
+InputHint.prototype.init = function() {
   //Set the value of the search input to the text of the label element
   //Add a class of "hint" to the search input
-  var _this = this;
   this.setHint();
 
   //Remove the label element
   this.label.remove();
 
-  //Bind a focus event to the search input that removes the hint text and the "hint" class
-  this.searchBox.on('focus', function() {
-    _this.removeHint();
-  });
-
-  //Bind a blur event to the search input that restores the hint text and "hint" class
-  //if no search text was entered
-  this.searchBox.on('blur', function() {
-    if(!_this.searchBox.val().trim()) {
-      _this.setHint();
-    }
-  });
+  this.bindEvents();
 }
 $(function() {
   var hintElements = {
@@ -42,5 +49,5 @@ $(function() {
   }
   var inputHint = new InputHint(hintElements);
 
-  inputHint.manageInputHint();
+  inputHint.init();
 });
