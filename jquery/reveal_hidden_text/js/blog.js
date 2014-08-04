f8308dcd754339fb9850212842f5d36d7226d1a7
shiksha <shiksha@vinsol.com>
2014-08-04 16:15:45 +0530

    Remove chaining with assignment and change naming

--- jquery/reveal_hidden_text/js/blog.js
+++ jquery/reveal_hidden_text/js/blog.js
@@ -2,34 +2,33 @@
 *
 *Clicking on another headline should slide down its excerpt paragraph,
 *and slide up any other currently showing excerpt paragraphs.*/
-function AddInteractivity(htmlElements) {
-  this.elements = htmlElements.elements;
-  this.slidedDownElement = htmlElements.elements.find('p.excerpt').first();
+function AddBlogInteractivity(htmlElements) {
+  this.blogElements = htmlElements.blogElements;
+  this.slidedDownElement = htmlElements.blogElements.find('p.excerpt :first-child');
 }
 
-AddInteractivity.prototype.slideDownElement = function(event) {
+AddBlogInteractivity.prototype.slideDownElement = function(event) {
   event.preventDefault();
 
   //slideUp currently slided down Element
   this.slidedDownElement.slideUp();
 
   //change value of currently slided down Element and slide it down
-  this.slidedDownElement = $(event.currentTarget)
-    .find('p.excerpt')
-      .slideDown();
+  this.slidedDownElement = $(event.currentTarget).find('p.excerpt');
+  this.slidedDownElement.slideDown();
 }
 
-AddInteractivity.prototype.bindEvents = function() {
+AddBlogInteractivity.prototype.bindEvents = function() {
   var _this = this;
-  this.elements.on('click', function(event) {
+  this.blogElements.on('click', function(event) {
     _this.slideDownElement(event);
   });
 }
 
 $(function() {
-  var blogElements = {
-    elements: $('#blog li')
+  var htmlElements = {
+    blogElements: $('#blog li')
   };
-  var addBlogInteractivity = new AddInteractivity(blogElements);
+  var addBlogInteractivity = new AddBlogInteractivity(htmlElements);
   addBlogInteractivity.bindEvents();
 });
