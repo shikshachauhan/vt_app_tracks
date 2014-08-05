b849e6b63e2eed425ba5641b9c7a7ddb2097282a
shiksha <shiksha@vinsol.com>
2014-08-05 16:50:51 +0530

    Use object orientation

--- jquery/arithmetic_quiz/js/arithmetic_quiz.js
+++ jquery/arithmetic_quiz/js/arithmetic_quiz.js
@@ -1,47 +1,29 @@
-/*Create a quiz, which asks 20 mathematical questions to the participant, one at a time.
-*
-*The quiz should generate two random numbers less than 20 and then pick an operator randomly 
-*( add, subtract, divide, multiply)
-*
-*Ask the question to the user and evaluate it when the user moves to the next screen 
-*and show the score at the bottom.
-*
-*User should not be allowed to go back and edit answers.
-*
-*Show them the final score and correct answers for any wrong question on the last screen.*/
-
-function ArithmeticQuiz(htmlElements) {
-  this.operators = ['+', '-', '/', '*'];
-  this.wrongAnswers = [];
+var ArithmeticQuiz =function(htmlElements) {
   this.score = 0;
-  this.questionsAsked = 0;
-  this.question = htmlElements.question;
+  this.questionsAsked = [];
   this.result = htmlElements.result;
+  this.question = htmlElements.question;
   this.scoreField = htmlElements.score;
   this.next = htmlElements.next;
   this.totalQuestions = htmlElements.totalQuestions;
 }
 
-ArithmeticQuiz.prototype.evaluateExpression = function() {
-  this.correctResult = eval(this.currentQuestion);
-};
-
 ArithmeticQuiz.prototype.updateScore = function() {
-  if(this.correctResult == Number(this.result.val())) {
+  var inputAnswer = Number(this.result.val());
+  if(this.currentQuestion.answer == inputAnswer) {
     this.score++;
   }
-  else {
-    this.wrongAnswers.push(this.currentQuestion + '=' + this.correctResult)
-  }
+  this.currentQuestion.storeInput(inputAnswer);
   this.scoreField.text(this.score);
 };
 
 ArithmeticQuiz.prototype.createQuestion = function() {
-  this.currentQuestion = Math.floor(Math.random() * 20) +
-    this.operators[Math.floor(Math.random() * 4)] + Math.floor(Math.random() * 20);
-    this.question.text(this.currentQuestion);
-    this.result.val('');
-    this.questionsAsked++;
+  var newQuestion = new Question();
+  this.questionsAsked.push(newQuestion);
+  newQuestion.init();
+  this.currentQuestion = newQuestion;
+  this.question.text(this.currentQuestion.question);
+  this.result.val('');
 };
 
 ArithmeticQuiz.prototype.showResult = function() {
@@ -50,23 +32,24 @@ ArithmeticQuiz.prototype.showResult = function() {
   var _this = this;
   $('body').append(function() {
     var wrongAnswerList = '<div>You gave Following incorrect answers<br/><br/></div>';
-    $.each(_this.wrongAnswers, function() {
-      wrongAnswerList += '<div>' + this + '</div>';
+    $.each(_this.questionsAsked, function() {
+      if(this.answer != this.inputAnswer) {
+        wrongAnswerList += '<div>' + this.display() + '</div>';
+      }
     })
     return '<div>' + wrongAnswerList + '</div>';
   });
 };
 
 ArithmeticQuiz.prototype.checkOverflow = function() {
-    if(this.questionsAsked == this.totalQuestions) {
-      this.showResult();
-    } else {
-      return false;
-    }
+  if(this.questionsAsked.length == this.totalQuestions) {
+    this.showResult();
+  } else {
+    return false;
+  }
 };
 
 ArithmeticQuiz.prototype.setNewQuestion = function() {
-  this.evaluateExpression();
   this.updateScore();
   if(!this.checkOverflow()) {
     this.createQuestion();
@@ -81,15 +64,3 @@ ArithmeticQuiz.prototype.init = function() {
     _this.setNewQuestion();
   });
 };
-
-$(function() {
-  var quizElements = {
-    question: $('#question'),
-    result: $('input[name="result"]'),
-    score: $('#score'),
-    next: $('#next-button'),
-    totalQuestions: 20
-  };
-  var arithmeticQuiz = new ArithmeticQuiz(quizElements);
-  arithmeticQuiz.init();
-});
