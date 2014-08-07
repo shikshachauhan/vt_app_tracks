bbe69ce6ff74b440d210e7e5daab42c50cce34db
shiksha <shiksha@vinsol.com>
2014-08-07 13:00:09 +0530

    Handle overflow properly and add uncheckAll day method

--- java_script/maximum_three_checkbox/js/maximum_three.js
+++ java_script/maximum_three_checkbox/js/maximum_three.js
@@ -3,36 +3,48 @@ function CheckboxGroup(checkBoxes, none) {
   this.none = none ;
   this.checkedBoxes = [];
 }
+
+CheckboxGroup.prototype.uncheckAllDay = function() {
+  var checkBoxesCount = this.checkBoxes.length;
+  for(var i = 0; i < checkBoxesCount; i++) {
+    this.checkBoxes[i].checked = false;
+  }
+};
+
 CheckboxGroup.prototype.alertMessage = function() {
   alert('Only 3 days can be selected.\nYou have already selected '
     + this.checkedBoxes[0].value + ', ' + this.checkedBoxes[1].value +
     ' and ' + this.checkedBoxes[2].value);
 }
+
 CheckboxGroup.prototype.addChecked = function(event) {
   this.none.checked = false;
-  this.checkedBoxes.push(event.target);
-  if(this.checkOverflow(event)) {
-    this.removeChecked(event)
+  if(!this.checkOverflow(event)) {
+    this.checkedBoxes.push(event.target.id);
   }
 }
+
 CheckboxGroup.prototype.checkOverflow = function(event) {
-  if(this.checkedBoxes.length == 4) {
+  if(this.checkedBoxes.length == 3) {
     event.target.checked = false;
     this.alertMessage();
     return true;
   }
   return false;
 }
+
 CheckboxGroup.prototype.removeChecked = function(event) {
   var index = this.checkedBoxes.indexOf(event.target);
   this.checkedBoxes.splice(index, 1);
 }
-CheckboxGroup.prototype.uncheckAll = function() {
+
+CheckboxGroup.prototype.uncheckAllChecked = function() {
   for(var i in this.checkedBoxes) {
-    this.checkedBoxes[i].checked = false;
+    document.getElementById(this.checkedBoxes[i]).checked = false;
   }
   this.checkedBoxes = [];
 }
+
 CheckboxGroup.prototype.manageCheckBoxGroup = function(event) {
   if(event.target.checked) {
     this.addChecked(event);
@@ -41,6 +53,7 @@ CheckboxGroup.prototype.manageCheckBoxGroup = function(event) {
     this.removeChecked(event);
   }
 }
+
 CheckboxGroup.prototype.bindEvents = function() {
   var groupSize = this.checkBoxes.length,
       checkboxGroup = this;
@@ -50,12 +63,15 @@ CheckboxGroup.prototype.bindEvents = function() {
     });
   }
   this.none.addEventListener('change', function() {
-    checkboxGroup.uncheckAll();
+    checkboxGroup.uncheckAllChecked();
   });
 }
+
 CheckboxGroup.prototype.init = function() {
   this.none.checked = true;
+  this.uncheckAllDay();
 }
+
 window.onload = function() {
   var days = document.getElementsByName('day'),
       weekDays = new CheckboxGroup(days, none);
